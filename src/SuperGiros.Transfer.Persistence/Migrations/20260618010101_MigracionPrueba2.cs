2d006ef61f5b8185c0f50ca5b80206b836ab4285
DesarrolloAY <ymollob@gmail.com>
2026-06-18 02:56:47 -0500

    base de datos

---
+++ src/SuperGiros.Transfer.Persistence/Migrations/20260618010101_MigracionPrueba2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperGiros.Transfer.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionPrueba2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
